11cc26d9e6516b8bcf715666b8726c79172dae5a
Katie Hinkle <katie@Katies-MacBook-Air.local>
2020-02-01 12:58:45 -0800

    changed code to where buttons are apart of the same function which is user and console log whatever the user chooses from the buttons is selected.

--- app.js
+++ app.js
@@ -1,21 +1,8 @@
-let rock = document.getElementById("rock")
-let paper = document.getElementById("paper")
-let scissors = document.getElementById("scissors")
+let rock = document.getElementById("rock").onclick = user;
+let paper = document.getElementById("paper").onclick = user;
+let scissors = document.getElementById("scissors").onclick = user;
 
-rock.addEventListener("click", rockevent);
-function rockevent() {
-    rock.style.color = "green";
-    rock.innerHTML = "You picked rock"
-}
-
-paper.addEventListener("click", paperevent);
-function paperevent() {
-    paper.style.color = "green";
-    paper.innerHTML = "You picked paper"
-}
-
-scissors.addEventListener("click", scissorevent);
-function scissorevent() {
-    scissors.style.color = "green";
-    scissors.innerHTML = "You picked scissors"
+function user(){
+    let userChoice = this.id;
+    console.log("User: " + userChoice)
 }
