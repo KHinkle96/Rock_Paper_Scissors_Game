a770dbd3a9f16b83a27f16718cc320c74bcc6ea2
Katie Hinkle <katie@Katies-MacBook-Air.local>
2020-02-01 13:16:04 -0800

    added the computer's pick to where it would be a random number and if the number is less than a certain number is how it will determine rock, paper, and scissors

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ let paper = document.getElementById("paper").onclick = user;
 let scissors = document.getElementById("scissors").onclick = user;
 
 function user(){
-    let userChoice = this.id;
-    console.log("User: " + userChoice)
+    let userPick = this.id;
+    console.log("User: " + userPick)
+
+    let computerPick = Math.random();
+    if (computerPick < 0.25) {
+        computerPick = "rock";
+    }else if (computerPick <= 0.6) {
+        computerPick = "paper";
+    }else{
+        computerPick = "scissors";
+
+    };
+
+    console.log("Computer: "+ computerPick);
 }
